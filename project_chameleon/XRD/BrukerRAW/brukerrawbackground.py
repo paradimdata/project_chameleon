8f57bf0db83cd08b67944481724415ae6f71427a
pcauchy1 <144147963+pcauchy1@users.noreply.github.com>
2024-03-21 21:53:02 -0400

    Cleaning
    Cleared uneccessary and redundant files

--- project_chameleon/XRD/BrukerRAW/brukerrawbackground.py
+++ project_chameleon/XRD/BrukerRAW/brukerrawbackground.py
@@ -7,60 +7,61 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import xylib
 
-file_type = input("Which file would you like to graph? \n 1 == .csv \n 2 == .raw \n")
+def brukerrawbackground(background_input, sample_input):
 
-if file_type == 1:
-    Background = pd.read_csv(input("Please input the name of your background file (include .csv) \n"))
-    Sample = pd.read_csv(input("Please input the name of your sample file (include .csv) \n"))
-elif file_type == 2:
-    background_input = input("Please input the name of your background file (do not include .raw) \n")
-    sample_input = input("Please input the name of your sample file (do not include .raw) \n")
-    background_name = background_input + 'text.txt'
-    sample_name = sample_input + 'text.txt'
-    brukerrawconverter(background_input + '.raw', background_name)
-    brukerrawconverter(sample_input + '.raw', sample_name)
-    Background = pd.read_csv(background_name, sep=' ', header=None, names=['Time','Measurement'])
-    Sample = pd.read_csv(sample_name, sep=' ', header=None, names=['Time','Measurement'])
+    if background_input.endswith('.csv'):
+        Background = pd.read_csv(background_input)
+    elif background_input.endswith('.raw'):
+        background_name = background_input + 'text.txt'
+        brukerrawconverter(background_input, background_name)
+        Background = pd.read_csv(background_name, sep=' ', header=None, names=['Time','Measurement'])
 
-plt.plot(Background.iloc[:,0], Background.iloc[:,1], label='Background')
-plt.plot(Sample.iloc[:,0], Sample.iloc[:,1], label='Sample')
-plt.legend()
-plt.title('Raw Data')
-plt.xlabel('Two Theta (Degrees)')
-plt.ylabel('Intensity (Arb. Units)')
-plt.show()
+    if sample_input.endswith('.csv'):
+        Sample = pd.read_csv(sample_input)
+    elif sample_input.endswith('.raw'):
+        sample_name = sample_input + 'text.txt'
+        brukerrawconverter(sample_input, sample_name)
+        Sample = pd.read_csv(sample_name, sep=' ', header=None, names=['Time','Measurement'])   
 
-mult = 0.019
-back_adj= Background.copy()
-back_adj.iloc[:,1] = back_adj.iloc[:,1].apply(lambda x: x*mult) 
+    plt.plot(Background.iloc[:,0], Background.iloc[:,1], label='Background')
+    plt.plot(Sample.iloc[:,0], Sample.iloc[:,1], label='Sample')
+    plt.legend()
+    plt.title('Raw Data')
+    plt.xlabel('Two Theta (Degrees)')
+    plt.ylabel('Intensity (Arb. Units)')
+    plt.show()
 
-fig, ax = plt.subplots()
+    mult = 0.019
+    back_adj= Background.copy()
+    back_adj.iloc[:,1] = back_adj.iloc[:,1].apply(lambda x: x*mult) 
 
-plt.plot(back_adj.iloc[:,0], back_adj.iloc[:,1], label='Adjusted Background')
-plt.plot(Sample.iloc[:,0], Sample.iloc[:,1], label='Sample')
-plt.legend(loc='upper left')
-plt.title('Background Adjusted Raw Data')
-plt.xlabel('Two Theta (Degrees)')
-plt.ylabel('Intensity (Arb. Units)')
+    fig, ax = plt.subplots()
 
-x1, x2, y1, y2 = 60, 150, 0, 800  # subregion of the original image
-axins = ax.inset_axes(
-    [0.5, 0.5, 0.47, 0.47],
-    xlim=(x1, x2), ylim=(y1, y2))
-axins.plot(back_adj.iloc[:,0], back_adj.iloc[:,1], label='Background')
-axins.plot(Sample.iloc[:,0], Sample.iloc[:,1], label='BCAVO')
+    plt.plot(back_adj.iloc[:,0], back_adj.iloc[:,1], label='Adjusted Background')
+    plt.plot(Sample.iloc[:,0], Sample.iloc[:,1], label='Sample')
+    plt.legend(loc='upper left')
+    plt.title('Background Adjusted Raw Data')
+    plt.xlabel('Two Theta (Degrees)')
+    plt.ylabel('Intensity (Arb. Units)')
 
-ax.indicate_inset_zoom(axins, edgecolor='black')
-plt.show()
+    x1, x2, y1, y2 = 60, 150, 0, 800  # subregion of the original image
+    axins = ax.inset_axes(
+        [0.5, 0.5, 0.47, 0.47],
+        xlim=(x1, x2), ylim=(y1, y2))
+    axins.plot(back_adj.iloc[:,0], back_adj.iloc[:,1], label='Background')
+    axins.plot(Sample.iloc[:,0], Sample.iloc[:,1], label='BCAVO')
 
-raw_diff= back_adj.copy()
-raw_diff.iloc[:,1] = Sample.iloc[:,1] - back_adj.iloc[:,1]
+    ax.indicate_inset_zoom(axins, edgecolor='black')
+    plt.show()
 
-plt.plot(raw_diff.iloc[:,0], raw_diff.iloc[:,1], label='Sample Subtracted')
-plt.legend()
-plt.title('Background Subtracted Raw Data')
-plt.xlabel('Two Theta (Degrees)')
-plt.ylabel('Intensity (Arb. Units)')
-plt.show()
+    raw_diff= back_adj.copy()
+    raw_diff.iloc[:,1] = Sample.iloc[:,1] - back_adj.iloc[:,1]
 
-raw_diff.to_csv('SAMPLE_backgroundSubtracted.csv')
+    plt.plot(raw_diff.iloc[:,0], raw_diff.iloc[:,1], label='Sample Subtracted')
+    plt.legend()
+    plt.title('Background Subtracted Raw Data')
+    plt.xlabel('Two Theta (Degrees)')
+    plt.ylabel('Intensity (Arb. Units)')
+    plt.show()
+
+    raw_diff.to_csv('SAMPLE_backgroundSubtracted.csv')
