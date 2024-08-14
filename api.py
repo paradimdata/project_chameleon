8372005940a3dc8af83af02eaedd0cb339ed3375
pcauchy1 <144147963+pcauchy1@users.noreply.github.com>
2024-08-14 13:16:07 -0400

    Update api.py

--- api.py
+++ api.py
@@ -23,7 +23,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
