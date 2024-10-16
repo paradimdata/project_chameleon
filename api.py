1d2b9185dc4b5cb4cf3f1323128b1d5d144c2825
pcauchy1 <144147963+pcauchy1@users.noreply.github.com>
2024-10-16 15:22:43 -0400

    Separated non4dstem file and folder functions in API

--- api.py
+++ api.py
@@ -153,7 +153,7 @@ def brukerbackground_convert_route(request: Request, data: dict = Body(...), acc
         return response
     elif request.method == 'POST':
         #EXCEPTIONS
-        if not (('background_file_name' in data) ^ ('background_file_bytes' in data) ^ ('background_file_url' in data)) or not (('sample_file_name' in data) ^ ('sample_file_bytes' in data) ^ ('sample_file_url' in data)) or 'output_file' not in data:
+        if not (('background_file_name' in data) ^ ('background_file_bytes' in data) ^ ('background_file_url' in data)) or not (('file_name' in data) ^ ('file_bytes' in data) ^ ('file_url' in data)) or 'output_file' not in data:
             raise HTTPException(status_code=400, detail='Incorrect number of parameters')
         
         if 'output_type' in data and all(opt not in data['output_type'] for opt in ['JSON', 'raw', 'file']):
@@ -164,9 +164,9 @@ def brukerbackground_convert_route(request: Request, data: dict = Body(...), acc
                 if not '.csv' in data.get('background_input_type'):
                     raise HTTPException(status_code=400, detail='Incorrect file extension: background_input_type options are .raw and .csv')
         
-        if 'sample_input_type' in data:
-            if not '.raw' in data.get('sample_input_type'):
-                if not '.csv' in data.get('sample_input_type'):
+        if 'input_type' in data:
+            if not '.raw' in data.get('nput_type'):
+                if not '.csv' in data.get('input_type'):
                     raise HTTPException(status_code=400, detail='Incorrect file extension: sample_input_type options are .raw and .csv')
                 
         auth_data = dict(data)
@@ -181,16 +181,16 @@ def brukerbackground_convert_route(request: Request, data: dict = Body(...), acc
         sample_ext = '.raw'
         if 'background_input_type' in data:
             background_ext = data.get('background_input_type')
-        if 'sample_input_type' in data:
-            sample_ext = data.get('sample_input_type')
+        if 'input_type' in data:
+            sample_ext = data.get('input_type')
 
         if 'background_file_name' in data:
             background = data.get('background_file_name')
             if not os.path.isfile(background):
                 raise HTTPException(status_code=400, detail='Local path is not a valid file')
 
-        if 'sample_file_name' in data:    
-            sample = data.get('sample_file_name')
+        if 'file_name' in data:    
+            sample = data.get('file_name')
             if not os.path.isfile(sample):
                 raise HTTPException(status_code=400, detail='Local path is not a valid file')
             
@@ -203,8 +203,8 @@ def brukerbackground_convert_route(request: Request, data: dict = Body(...), acc
             os.rename(temp_file.name, background)
             
 
-        if 'sample_file_bytes' in data:
-            sample_file_bytes = data.get('sample_file_bytes')
+        if 'file_bytes' in data:
+            sample_file_bytes = data.get('file_bytes')
             decoded_data = base64.b64decode(sample_file_bytes)
             with tempfile.NamedTemporaryFile(delete=False) as temp_file:
                 temp_file.write(decoded_data)
@@ -216,17 +216,17 @@ def brukerbackground_convert_route(request: Request, data: dict = Body(...), acc
             urllib.request.urlretrieve(background_file_url, filename = 'background_temp_name' + background_ext) 
             background = 'background_temp_name' + background_ext
 
-        if 'sample_file_url' in data:
-            sample_file_url = data.get('sample_file_url')
+        if 'file_url' in data:
+            sample_file_url = data.get('file_url')
             urllib.request.urlretrieve(sample_file_url, filename = 'sample_temp_name' + sample_ext) 
             sample = 'sample_temp_name' + sample_ext
 
         output_file = data.get('output_file')
         result = brukerrawbackground(background, sample, output_file)
 
-        if ('background_file_bytes' in data) or ('background_file_url' in data):
+        if ('background_file_bytes' in data) or ('file_url' in data):
             os.remove(background)
-        if ('sample_file_bytes' in data) or ('background_file_url' in data):
+        if ('file_bytes' in data) or ('background_file_url' in data):
             os.remove(sample)
 
         #OUTPUT
@@ -475,8 +475,8 @@ def MBE_parser_route(request: Request, data: dict = Body(...), access_token: str
         else:
             raise HTTPException(status_code=500, detail=f'Failed to convert file')
     
-@app.post('/non4dstem')
-def non4dstem_convert_route(request: Request, data: dict = Body(...), access_token: str = Header(default=''), x_auth_access_token: str = Header(default='')):
+@app.post('/non4dstem_folder')
+def non4dstem_folder_convert_route(request: Request, data: dict = Body(...), access_token: str = Header(default=''), x_auth_access_token: str = Header(default='')):
 
     try:
         if 'access_token' in data:
@@ -511,17 +511,16 @@ def non4dstem_convert_route(request: Request, data: dict = Body(...), access_tok
 
         #INPUTS
         result = None
+        output = data.get('output_folder')
         if 'folder_name' in data:
             file_folder = data.get('folder_name')
-            output_folder = data.get('output_folder')
 
             if not os.path.isdir(file_folder):
                 raise HTTPException(status_code=400, detail='Local path is not a valid directory')
-            result = non4dstem(file_folder, output_folder)
+            result = non4dstem(data_folder = file_folder, output_folder = output)
         
         if 'folder_bytes' in data:
             folder_bytes = data.get('folder_bytes')
-            output_folder = data.get('output_folder')
 
             decoded_data = base64.b64decode(folder_bytes)
             with tempfile.NamedTemporaryFile(delete=False) as temp_folder:
@@ -533,44 +532,43 @@ def non4dstem_convert_route(request: Request, data: dict = Body(...), access_tok
             with zipfile.ZipFile(temp_name, 'r') as zip_ref:
                 zip_ref.extractall('temp_dir')
             folder = 'temp_dir/' + os.listdir('temp_dir')[0]
-            result = non4dstem(folder,output_folder)
+            result = non4dstem(data_folder = folder,output_folder = output)
             shutil.rmtree('temp_dir')
 
         if 'folder_url' in data:
             folder_url = data.get('folder_url')
-            output_folder = data.get('output_folder')
 
             urllib.request.urlretrieve(folder_url, filename = 'non4dstem_data.zip')
             with zipfile.ZipFile('non4dstem_data.zip', 'r') as zip_ref:
                 zip_ref.extractall('temp_dir') 
             folder = 'temp_dir/' + os.listdir('temp_dir')[0]
-            result = non4dstem(folder, output_folder)
+            result = non4dstem(data_folder = folder, output_folder = output)
             shutil.rmtree('temp_dir')
 
         #OUTPUTS
         if 'output_type' in data:
             if data.get('output_type') == 'raw':
                 with zipfile.ZipFile('non4dstem_output.zip', 'w') as zipf:
-                    for root, dirs, files in os.walk(output_folder):
+                    for root, dirs, files in os.walk(output):
                         for file in files:
                             file_path = os.path.join(root, file)
-                            zipf.write(file_path, os.path.relpath(file_path, output_folder))
+                            zipf.write(file_path, os.path.relpath(file_path, output))
                 with open('non4dstem_output.zip', 'rb') as file:
                     encoded_data = base64.b64encode(file.read()).decode('utf-8')
                     out = encoded_data
-                    shutil.rmtree(output_folder)
+                    shutil.rmtree(output)
             elif data.get('output_type') == 'JSON':
                 with zipfile.ZipFile('non4dstem_output.zip', 'w') as zipf:
-                    for root, dirs, files in os.walk(output_folder):
+                    for root, dirs, files in os.walk(output):
                         for file in files:
                             file_path = os.path.join(root, file)
-                            zipf.write(file_path, os.path.relpath(file_path, output_folder))
+                            zipf.write(file_path, os.path.relpath(file_path, output))
                 with open('non4dstem_output.zip', 'rb') as file:
                     encoded_data = base64.b64encode(file.read()).decode('utf-8')
                     with open('non4dstem_out_json', 'w') as json_file:
                         json.dump({"file_data": encoded_data}, json_file)
                         out = json_file
-                shutil.rmtree(output_folder)
+                shutil.rmtree(output)
             else:
                 out = None
         else:
@@ -581,6 +579,98 @@ def non4dstem_convert_route(request: Request, data: dict = Body(...), access_tok
                 return {'message': 'Images converted successfully'}, out
             else:
                 return {'message': 'Images converted successfully'}
+        else:
+            raise HTTPException(status_code=500, detail=f'Failed to convert folder')
+
+@app.post('/non4dstem_file')
+def non4dstem_file_convert_route(request: Request, data: dict = Body(...), access_token: str = Header(default=''), x_auth_access_token: str = Header(default='')):
+
+    try:
+        if 'access_token' in data:
+            # JSON overrides header
+            access_token = str(data['access_token'])
+        elif len(access_token) == 0:
+            access_token = x_auth_access_token
+    except:
+        raise HTTPException(status_code=400, detail='Malformed parameters')
+
+    if request.method == 'OPTIONS':
+        # Handle preflight requests
+        response = app.make_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, access-token'
+        return response
+    elif request.method == 'POST':
+        #EXCEPTIONS
+        if not (('file_name' in data) ^ ('file_bytes' in data) ^ ('file_url' in data)) or 'output_file' not in data:
+            raise HTTPException(status_code=400, detail='Incorrect number of parameters')
+        
+        if 'output_type' in data and all(opt not in data['output_type'] for opt in ['JSON', 'raw', 'file']):
+                raise HTTPException(status_code=400, detail='Incorrect output_type: output_type options are raw, JSON, file')
+
+        auth_data = dict(data)
+        if 'folder_bytes' in data:
+            del auth_data['folder_bytes']
+
+        if not authorized(access_token, "org.paradim.data.api.v1.chameleon", auth_data):
+            raise HTTPException(status_code=401, detail='Unauthorized')
+
+        #INPUTS
+        file_ext = '.emd'
+        if 'file_input_type' in data:
+            file_ext = data.get('file_input_type')
+        output = data.get('output_file')
+        if 'file_name' in data:
+            file_name = data.get('file_name')
+
+            if not os.path.isfile(file_name):
+                raise HTTPException(status_code=400, detail='Local path is not a valid file')
+            result = non4dstem(data_file = file_name, output_file = output)
+        
+        if 'file_bytes' in data:
+            file_bytes = data.get('file_bytes')
+
+            decoded_data = base64.b64decode(file_bytes)
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(decoded_data)
+                temp_name = temp_file.name + file_ext
+            os.rename(temp_file.name, temp_name)
+            output_file = os.path.join(tempfile.gettempdir(), output_file)
+            result = non4dstem(data_file = temp_name, output_file = output)
+            os.remove(temp_name)
+
+        if 'file_url' in data:
+            file_url = data.get('file_url')
+
+            urllib.request.urlretrieve(file_url, filename = 'temp_name' + file_ext) 
+            result = non4dstem(data_file = 'temp_name' + file_ext, output_file = output)
+            os.remove('temp_name' + file_ext)
+
+        #OUTPUTS
+        if 'output_type' in data:
+            if data.get('output_type') == 'raw':
+                with open(output_file, 'rb') as file:
+                    encoded_data = base64.b64encode(file.read()).decode('utf-8')
+                    out = encoded_data
+                    os.remove(output_file)
+            elif data.get('output_type') == 'JSON':
+                with open(output_file, 'rb') as file:
+                    encoded_data = base64.b64encode(file.read()).decode('utf-8')
+                with open('ppms_out_json', 'w') as json_file:
+                    json.dump({"file_data": encoded_data}, json_file)
+                    out = json_file
+                os.remove(output_file)
+            else:
+                out = None
+        else:
+            out = None
+
+        if result is None:
+            if out:
+                return out
+            else:
+                return {'message': 'File converted successfully'}
         else:
             raise HTTPException(status_code=500, detail=f'Failed to convert file')
     
