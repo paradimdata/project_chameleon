26e5a8c10a1c4f0ddd857b7ea5938bdaa7d70c2f
pcauchy1 <144147963+pcauchy1@users.noreply.github.com>
2024-10-23 11:06:20 -0400

    Fixed hs2 tests

--- tests/rheed_script_test.py
+++ tests/rheed_script_test.py
@@ -3,38 +3,38 @@ import pathlib
 import shutil
 import imagehash
 from PIL import Image
-from project_chameleon.hs2converter import hs2converter
+from project_chameleon.rheedconverter import rheedconverter
 
 
-def test_hs2_basic_output():
+def test_rheed_basic_output():
     test_file_path = (
         pathlib.Path(__file__).parent
-        / test_hs2_basic_output.__name__
+        / test_rheed_hash.__name__
         / "test_file.png"
     )
     assert not test_file_path.parent.is_dir()
     test_file_path.parent.mkdir()
     try:
-        hs2converter('/project_chameleon/tests/data/hs2/test.hs2',test_file_path)
+        rheedconverter('/project_chameleon/tests/data/rheed/test.img',test_file_path)
         assert test_file_path.is_file()
     finally:
         shutil.rmtree(test_file_path.parent)
 
-def test_hs2_hash():
+def test_rheed_hash():
     test_file_path = (
         pathlib.Path(__file__).parent
-        / test_hs2_hash.__name__
+        / test_rheed_hash.__name__
         / "test_file.png"
     )
     assert not test_file_path.parent.is_dir()
     test_file_path.parent.mkdir()
-    hs2converter('/project_chameleon/tests/data/rheed/test.hs2',test_file_path)
-    hash0 = imagehash.average_hash(Image.open('hs2.png')) 
+    rheedconverter('/project_chameleon/tests/data/rheed/test.img',test_file_path)
+    hash0 = imagehash.average_hash(Image.open('new_test_output.png')) 
     hash1 = imagehash.average_hash(Image.open(test_file_path)) 
     shutil.rmtree(test_file_path.parent)
     assert hash1 - hash0 < 5
     
 def test_no_arguments():
     with pytest.raises(TypeError) as exc_info:
-        hs2converter()
+        rheedconverter()
     assert str(exc_info.value) == "rheedconverter() missing 2 required positional arguments: 'file_name' and 'output_file'"
