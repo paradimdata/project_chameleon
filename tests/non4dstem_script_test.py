39a4372c51a2bb3efef152c5295f8f6c08ea905f
pcauchy1 <144147963+pcauchy1@users.noreply.github.com>
2024-10-28 11:00:06 -0400

    Updated README

--- tests/non4dstem_script_test.py
+++ tests/non4dstem_script_test.py
@@ -5,17 +5,31 @@ import os
 from pathlib import Path
 from project_chameleon.non4dstem import non4dstem
 
-def test_non4dstem_basic_output():
+def test_non4dstem_basic_folder_output():
     non4dstem_file_path_basic = (
         pathlib.Path(__file__).parent
-        / test_non4dstem_basic_output.__name__
+        / test_non4dstem_basic_folder_output.__name__
         / "test_folder"
     )
     assert not non4dstem_file_path_basic.parent.is_dir()
     non4dstem_file_path_basic.parent.mkdir()
     try:
-        non4dstem('/data/non4dstem_data', non4dstem_file_path_basic)
+        non4dstem(data_folder = '/data/non4dstem_data', outputs_folder = non4dstem_file_path_basic)
         assert non4dstem_file_path_basic.is_dir()
+    finally:
+        shutil.rmtree(non4dstem_file_path_basic.parent)
+
+def test_non4dstem_basic_file_output():
+    non4dstem_file_path_basic = (
+        pathlib.Path(__file__).parent
+        / test_non4dstem_basic_file_output.__name__
+        / "test_file.png"
+    )
+    assert not non4dstem_file_path_basic.parent.is_dir()
+    non4dstem_file_path_basic.parent.mkdir()
+    try:
+        non4dstem(data_file = '/data/non4dstem_data/005_ADF Image (SI Survey).dm4', output_file = non4dstem_file_path_basic)
+        assert non4dstem_file_path_basic.is_file()
     finally:
         shutil.rmtree(non4dstem_file_path_basic.parent)
 
@@ -28,7 +42,7 @@ def test_non4dstem_file_output():
     assert not non4dstem_file_path_file.parent.is_dir()
     non4dstem_file_path_file.parent.mkdir()
     try:
-        non4dstem('/data/non4dstem_data', non4dstem_file_path_file)
+        non4dstem(data_folder = '/data/non4dstem_data', outputs_folder = non4dstem_file_path_file)
         files = os.listdir(non4dstem_file_path_file)
         assert files
 
