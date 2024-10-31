c3960fce375a122c61a7a99ffa85190ff3f4d3cb
pcauchy1 <144147963+pcauchy1@users.noreply.github.com>
2024-10-31 16:02:40 -0400

    Adding testing

---
+++ tests/api_test.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+import pytest
+import pathlib
+import shutil
+from pathlib import Path
+
+from api import app
+
+client = TestClient(app)
+
+def test_authorization_inputs():
+    response = client.get("/rheedconverter", headers={"access-token": "", "Content-Type": "application/json"}, data={"input_url":"https://github.com/paradimdata/project_chameleon/raw/main/tests/data/rheed/test.img","output_dest": "file","output_file": "urltest_out.png"})
+    assert response.status_code == 200
+    assert response.json() == {
+        "detail":"Unauthorized"
+    }
+
+def test_rheed_inputs():
+    response = client.get("/rheedconverter", headers={"access-token": "X9g218KJ9AwKG4KRPHbKUJzYK-FLBS8neybUEV6cO_w", "Content-Type": "application/json"}, data={"output_dest": "file","output_file": "urltest_out.png"})
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail":"Incorrect number of parameters"
+    }
--- tests/test_api.py
+++
@@ -1,4 +0,0 @@
-from fastapi.testclient import TestClient
-
-from api import app
-
