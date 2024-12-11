cb526f0cfb0298058befc88d74ee6d99f3bc4cb4
pcauchy1 <144147963+pcauchy1@users.noreply.github.com>
2024-12-11 12:43:17 -0500

    Update brukerrawbackground.py

--- project_chameleon/brukerrawbackground.py
+++ project_chameleon/brukerrawbackground.py
@@ -1,4 +1,4 @@
-from project_chameleon.brukerrawconverter import brukerrawconverter
+from brukerrawconverter import brukerrawconverter
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
