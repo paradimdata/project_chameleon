paradimdata/project_chameleon

commit 658c37b02e776769fc8fdcbb597e3ac4f64613ab

Update brukerrawbackground.py

project_chameleon/brukerrawbackground.py
@@ -1,4 +1,4 @@
-from .brukerrawconverter import brukerrawconverter
+from brukerrawconverter import brukerrawconverter
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt

commit e6ccdaae4363c149c19992ba29afd2ab633a4fe7

Fix relative package imports broken by commit 658c37b.

project_chameleon/brukerrawbackground.py
@@ -1,4 +1,4 @@
-from brukerrawconverter import brukerrawconverter
+from .brukerrawconverter import brukerrawconverter
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt