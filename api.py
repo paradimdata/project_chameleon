cec38fd6ec725632984816bc2ff74d529fe93d77
pcauchy1 <144147963+pcauchy1@users.noreply.github.com>
2025-03-19 12:46:11 -0400

    Update api.py

--- api.py
+++ api.py
@@ -32,7 +32,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
